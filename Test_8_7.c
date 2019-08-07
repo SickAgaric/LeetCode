f8c5ccd9fc138d55838036a25a5b571bbbb61634
mochu823@yeah.net <mochu823@yeah.net>
2019-08-07 23:15:29 +0800

    提交代码

---
+++ Test_8_7.c
@@ -0,0 +1,7 @@
+int removeDuplicates(int* nums, int numsSize){
+    int i=0;
+    for(int j=0;j<numsSize;j++)
+        if(i<2 || nums[j] > nums[i-2])
+            nums[i++] = nums[j];
+    return i;
+}
