231f2ed93427b089102287608ed73ad4690b65ae
mochu823@yeah.net <mochu823@yeah.net>
2019-10-09 22:43:06 +0800

    提交代码

--- Test_10_9.c
+++ Test_10_9.c
@@ -6,3 +6,27 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+       
+        int sum1 = 0;
+        int sum2 = 0;
+        auto n = nums.begin();
+        
+        while(n!=nums.end())
+        {
+            sum1 += *n;
+            ++n;
+        }
+        
+        for(int i = 0;i<=nums.size();i++)
+            sum2 += i;
+        
+        
+        return sum2-sum1;
+    }
+};
