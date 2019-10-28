1982d3961c12bc8f7c80988761d178b9643fd326
mochu823@yeah.net <mochu823@yeah.net>
2019-10-28 23:34:19 +0800

    a

---
+++ Test_10_28.c
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        map<string,vector<string>> M;
+        for(int i=0;i<strs.size();i++){
+            string key=strs[i];
+            sort(key.begin(),key.end());
+            M[key].push_back(strs[i]);
+        }
+        for(auto ite=M.begin();ite!=M.end();ite++)
+            res.push_back(ite->second);
+        return res;
+    }
+};
