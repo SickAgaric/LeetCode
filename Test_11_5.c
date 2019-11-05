1a498c5a62adace1aa5987a2ba58e2b73bccce33
mochu823@yeah.net <mochu823@yeah.net>
2019-11-06 00:19:36 +0800

    a

--- Test_11_5.c
+++ Test_11_5.c
@@ -1,1 +1,29 @@
-fuck ultoa
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	int n;
+	while(cin>>n)
+	{
+		long long sum = 0;
+		vector<int> v;
+		for(int i = 0;i<n*3;i++)
+		{
+			cin >> v[i];
+		}
+		
+		sort(v.begin(),v.end());
+		
+		for(int i = n;i<n*3;i++)
+		{
+			sum+=v[i];
+		}
+		
+		cout << sum;
+	}
+	return 0;
+}
