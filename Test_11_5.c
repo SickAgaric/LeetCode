2e23ff163317ea45d4b3b9b07196203ee9cdff20
mochu823@yeah.net <mochu823@yeah.net>
2019-11-05 23:21:58 +0800

    a

--- Test_11_5.c
+++ Test_11_5.c
@@ -1,40 +1,9 @@
-#include<iostream>
 
-
-using namespace std;
-#define MAX(a, b)       (((a) > (b) ) ? (a) : (b))
-#define MIN(a, b)       (((a) < (b) ) ? (a) : (b))
-#define MID(a,b,c)      (MAX(a,b)>c?MAX(MIN(a,b),c):MIN(MAX(a,b),c))
-
-
-int main()
-{
-    int n;
-    cin >> n;
-    
-    int len = 3*n;
-    int* a = new int [len];
-    cin >> a >> endl;
-    
-    int i = 0;
-    int* b = new int [3];
-    int result = 0;
-    while(i<len)
-    {
-        for(int i = 0;i < 3;i++)
-        {
-            b[i] = a[i];
-        }
-        result += MID(b[i],b[i+1],b[i+2]);
-        
-        i+=3;
-    }
-    return 0;
-}
 
 #include<iostream>
 #include<vector>
 #include<algorithm>
+
 
 using namespace std;
 
