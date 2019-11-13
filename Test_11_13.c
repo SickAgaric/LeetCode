827dcc9d4ae9e73ab2c9571e2937a60435dda5b2
mochu823@yeah.net <mochu823@yeah.net>
2019-11-13 20:27:21 +0800

    a

--- Test_11_13.c
+++ Test_11_13.c
@@ -44,3 +44,99 @@ public:
         return (count == 0);
     }
 };
+
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+bool Lex_Gra(vector<string> str)
+{
+    int i = 0;
+    
+    while((i+1) < str.size())
+    {
+        if(str[i].compare(str[i+1]) > 0)
+            return false;
+        i++;
+    }
+    return true;
+}
+
+bool Lengths(vector<string> str)
+{
+    /*int len = strlen(str[0]);
+    
+    for(int i = 1;i < str.size();i++)
+    {
+        if(len > strlen(str[i]))
+            return false;
+        len = strlen(strlen[i]);
+    }*/
+    int i = 0;
+    
+    while((i+1) < str.size())
+    {
+        if(str[i].size() > str[i+1].size())
+            return false;
+        i++;
+    }
+    return true;
+}
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    string s;
+    vector<string> str;
+    
+    for(int i = 0;i < n;i++)
+    {
+        cin >> s;
+        str.push_back(s);
+    }
+    
+    Lex_Gra(str);
+    Lengths(str);
+    
+    if(Lex_Gra(str) && Lengths(str))
+        cout << "both";
+    else if(Lengths(str) && !Lex_Gra(str))
+        cout << "lengths";
+    else if(!Lengths(str) && Lex_Gra(str))
+        cout << "lexicographically";
+    else
+        cout << "none";
+}
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int A,B;
+    
+    cin >> A >> B;
+    int i = 1;
+    int n = 1;
+    if(A<B)
+    {
+        int tmp = A;
+        A = B;
+        B = tmp;
+    }
+    while(i<A)
+    {
+        if(A%i == 0 && B%i == 0)
+            n = i;
+        i++;
+    }
+    
+    cout << (((A/n)*(B/n)*n));
+    return 0;
+}
