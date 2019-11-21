3ecd25d463bec8b8baeff046da84a1755c81a348
mochu823@yeah.net <mochu823@yeah.net>
2019-11-21 21:47:57 +0800

    a

--- Test_11_21.c
+++ Test_11_21.c
@@ -65,3 +65,27 @@ int main()
     
 	return 0;
 }
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int num;
+    while(cin >> num)
+    {
+        int count = 0;
+        while(num)
+        {
+            if(num & 1 == 1)
+                count++;
+            num = num >> 1;
+        }
+
+        cout << count << endl;
+    }
+
+        return 0;
+}
