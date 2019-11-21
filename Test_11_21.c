fa3560971987930f1b7ced879edbea1b42ad9b9f
mochu823@yeah.net <mochu823@yeah.net>
2019-11-21 22:15:50 +0800

    a

--- Test_11_21.c
+++ Test_11_21.c
@@ -89,3 +89,57 @@ int main()
 
         return 0;
 }
+
+class Gloves {
+public:
+    int findMinimum(int n, vector<int> left, vector<int> right) {
+        /*int r_count = 0;
+        int r_sum = 0;
+        int l_sum = 0;
+        int max = 0;
+        vector<int> list;
+        
+        for(int i = 0;i < right.size();++i)
+        {
+            if(right[i] == 0)
+            {
+                r_count++;
+                list.push_back(i);
+            }
+        }
+        
+        for(int k = 0;k < right.size();++k)
+        {
+            if(max < right[k])
+                max = right[k];
+        }
+        r_sum = max + right.size() - 1 - r_count;
+        
+        for(size_t j = 0;j < list.size();++j)
+        {
+            if(left[list[j]] != right[list[j]])
+                l_sum++;
+        }
+        
+        return (r_sum + l_sum + 1);*/
+		int l_sum = 0,l_min = INT_MAX,r_sum = 0,r_min = INT_MAX;
+        
+        int sum = 0;
+        
+        for(int i = 0;i < n;++i)
+        {
+            if(left[i]*right[i] == 0)
+                sum += left[i]+right[i];//左或者右不为0的数的总和
+            else
+            {
+                l_sum += left[i];
+                r_sum += right[i];
+                l_min = min(left[i],l_min);
+                r_min = min(right[i],r_min);
+            }
+        }
+        //sum + min(l_sum - l_min + 1,r_sum - r_min + 1)表示取某只手的最坏的结果，然后在另一只去一直，即可匹配
+        return sum + min(l_sum - l_min + 1,r_sum - r_min + 1) + 1;
+    }
+    }
+};
