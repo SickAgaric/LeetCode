bfa2f98e72cba89daf817b90e95c724494ddbbb5
mochu823@yeah.net <mochu823@yeah.net>
2019-12-03 22:55:22 +0800

    a

--- Test_12_3.c
+++ Test_12_3.c
@@ -16,3 +16,26 @@ public:
         return num;
     }
 };
+
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        int index_i = 0,index_j = 0;
+        
+        
+        for(size_t i = 0;i < array[0].size()-1;++i)
+            index_j++;
+        
+        while(index_j >= 0&&index_i < array.size())
+        {
+            if(array[index_i][index_j] > target)
+                index_j--;
+            else if(array[index_i][index_j] < target)
+                index_i++;
+            else
+                return true;
+        }
+        
+        return false;
+    }
+};
