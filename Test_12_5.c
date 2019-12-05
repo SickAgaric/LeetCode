b6b32cfa5eed70f84ddc2f1cc1247984873267ea
mochu823@yeah.net <mochu823@yeah.net>
2019-12-05 23:00:47 +0800

    a

--- Test_12_5.c
+++ Test_12_5.c
@@ -30,3 +30,72 @@ public:
         
     }
 };
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string string_sum(string& s1,string& s2)
+{
+    int len1 = s1.size() - 1;
+    int len2 = s2.size() - 1;
+    int t = 0;
+    string res;
+    int i,j;
+    for(i = len1,j = len2;i >= 0&&j >= 0;i--,j--)
+    {
+        t += ((s1[i] - '0') + (s2[j] - '0'));
+        res = (char)(t%10 + '0') + res;
+        t /= 10;
+    }
+    
+    while(i >= 0)
+    {
+        t += ((s1[i]) - '0');
+        res = (char)(t + '0') + res;
+        t /= 10;
+        i--;
+    }
+    
+    while(j >= 0)
+    {
+        t += ((s2[j]) - '0');
+        res = (char)(t + '0') + res;
+        t /= 10;
+        j--;
+    }
+    
+    if(t > 0)
+        res = (char)(t + '0') + res;
+    
+    return res;
+}
+
+bool compare(string s1,string s2)
+{
+    if(s1.size() > s2.size())
+        return true;
+    else if(s1.size() < s2.size())
+        return false;
+    else
+        return s1 > s2;
+}
+
+int main()
+{
+    string a,b,c;
+    while(cin >> a >> b >> c)
+    {
+        /*if(a.size() + b.size() > c.size() && a.size() - b.size() < c.size())
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;*/
+        
+        if(compare(string_sum(a,b),c) && compare(string_sum(b,c),a) &&compare(string_sum(a,c),b))//2.与第三项比较
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+}
