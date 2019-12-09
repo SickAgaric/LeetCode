2c9c64be7a5a5891b1a3935737b6b13fc6c53a09
mochu823@yeah.net <mochu823@yeah.net>
2019-12-09 21:30:33 +0800

    a

--- Test_12_9.c
+++ Test_12_9.c
@@ -24,3 +24,31 @@ public:
         return count;
     }
 };
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int j;
+	int n = 0;
+	cin >> n;
+	if (n >= 2)
+		cout << 2 << "  ";
+	for (int i = 2; i <= n; i++)
+	{
+		for (j = 2; j*j <= i; j++)
+		{
+			if (i%j == 0)
+				break;
+		}
+
+		if (i%j != 0)
+			cout << i << "  ";
+
+	}
+	system("pause");
+	return 0;
+}
