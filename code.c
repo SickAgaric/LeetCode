de4acdc4c42a502aef1337c3eb50e60c159239ee
MrC <50049963+SickAgaric@users.noreply.github.com>
2020-02-27 23:48:58 +0800

    Update code.c

--- code.c
+++ code.c
@@ -346,3 +346,68 @@ public:
         return pre;
     }
 };
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        if(pHead1 == NULL)
+            return pHead2;
+        else if(pHead2 == NULL)
+            return pHead1;
+        else if(pHead1 == NULL && pHead2 == NULL)
+            return NULL;
+        
+        ListNode* new_list = nullptr;
+        if(pHead1->val < pHead2->val)
+        {
+            new_list = pHead1;
+            pHead1 = pHead1->next;
+        }
+        else
+        {
+            new_list = pHead2;
+            pHead2 = pHead2->next;
+        }
+        
+        while(pHead1 && pHead2)
+        {
+            if(pHead1 < pHead2)
+            {
+                new_list->next = pHead1;
+                new_list = new_list->next;
+                new_list->next = pHead2;
+            }
+            else
+            {
+                new_list->next = pHead2;
+                new_list = new_list->next;
+                new_list->next = pHead1;
+            }
+            pHead1 = pHead1->next;
+            pHead2 = pHead2->next;
+        }
+        
+        if(pHead1)
+        {
+            new_list->next = pHead1;
+        }
+        else if(pHead2)
+        {
+            new_list->next = pHead2;
+        }
+        else
+        {
+            new_list->next = NULL;
+        }
+        
+        return new_list;
+    }
+};
