0b1ca6e06d0a459224ff1135b647eb252f10a3d0
MrC <50049963+SickAgaric@users.noreply.github.com>
2020-05-21 17:20:19 +0800

    Update code.c

--- code.c
+++ code.c
@@ -355,3 +355,55 @@ struct ListNode {
 			val(x), next(NULL) {
 	}
 };*/
+class Solution {
+public:
+	vector<int> arrayRankTransform(vector<int>& arr) {
+		set<int> sort_set;
+
+		for (auto e : arr)
+			sort_set.insert(e);
+
+		vector<int> newArr;
+		vector<int> indexArr;
+
+		for (auto e : sort_set)
+			newArr.push_back(e);
+
+		for (int i = 0; i < arr.size(); ++i)
+		{
+			int index = 0;
+			while (1)
+			{
+				if (arr[i] == newArr[index])
+				{
+					indexArr.push_back(index+1);
+					break;
+				}
+				index++;
+			}
+		}
+		return indexArr;
+	}
+};
+
+class Solution {
+public:
+    int NumberOf1Between1AndN_Solution(int n)
+    {
+        if(n < 1)
+            return 0;
+        
+        int count = 0;
+        for(int i = 1;i <= n;++i)
+        {
+            int tmp = i;
+            while(tmp)
+            {
+                if(tmp % 10 == 1)
+                    count++;
+                tmp /= 10;
+            }
+        }
+        return count;
+    }
+};
